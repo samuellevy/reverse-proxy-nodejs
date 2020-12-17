23dfe819535e6cba2cc1b8f42fdfb55f8a5f9879
Samuel Levy <samuellevy.dev@gmail.com>
2020-12-17 02:14:00 -0300

    add all env vars

--- src/server.ts
+++ src/server.ts
@@ -33,7 +33,7 @@ var verifySegAuth = function (req: Request, res: Response, next: NextFunction) {
 };
 
 app.get('/info', (req,res)=>{
-  return res.send({ message: DM_PROXY_SELF, DM_PROXY_SELF, DM_PROXY_REFERER, DM_PROXY_TARGET, DM_PROXY_AUTH});
+  return res.send({ message: process.env, DM_PROXY_SELF, DM_PROXY_REFERER, DM_PROXY_TARGET, DM_PROXY_AUTH});
 });
 // app.use(cors({origin: "http://acd", credentials: true,}))
 if(DM_PROXY_AUTH=='on'){
